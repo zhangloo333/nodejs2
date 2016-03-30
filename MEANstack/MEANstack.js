6abd3a4031e28d732db2c92561e48b78c732f23c
lee <zhangloo333@gmail.com>
2016-03-30 11:09:17 -0700

    update to MEANstack how to combination clien side and server side

--- MEANstack/MEANstack.js
+++ MEANstack/MEANstack.js
@@ -5,5 +5,11 @@ the combination of all teachnologies used to build a piece of software.
 dom: the structure browers use to store and manage web pages.
 
 brower-->read html as string--> build DOM tree: 把element 分离了。
+
+clien side 的jsscript 怎么和 nodeside的javascript作用。
+
+首先先从server side 把数据取到 clien side：就动态表达式 取到 clien side的<script><script>里面交个一个变量，
+然后在用 clien side的
+
 */
 
